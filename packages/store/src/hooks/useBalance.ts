6eca81421bfe81ee4de2b6b12bb5f02583dbe20f
spandan-mozumder <mozumder.spandan.professional@gmail.com>
2025-06-01 11:23:50 +0530

    full config init

---
+++ packages/store/src/hooks/useBalance.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
+import { balanceAtom } from "../atoms/balance";
+
+export const useBalance = () => {
+  const value = useRecoilValue(balanceAtom);
+  return value;
+};
---
+++ packages/ui/src/code.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export function Code({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}): React.JSX.Element {
+  return <code className={className}>{children}</code>;
+}
